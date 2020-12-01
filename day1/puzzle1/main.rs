e3a0e19f06b7e61c2b5b67380a572d25ccc5d2c5
Spoor2709 <james.spoor7@gmail.com>
2020-12-01 18:26:33 +0100

    day1

---
+++ day1/puzzle1/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+fn main(){
+  let input = fs::read_to_string("numbersFile.input")
+        .expect("Something went wrong reading the file");
+  let num_array: Vec<i32> = input.split("\n").into_iter().map(|x| x.parse::<i32>().unwrap()).collect();
+  println!("{:#?}",num_array);
+  for num in num_array.iter() {
+    for num_2 in num_array.iter() {
+      if *num + *num_2 == 2020 {
+        println!("the answer is: {:#?}",num * num_2)
+      }
+    }
+  }
+}
